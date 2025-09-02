82d9901978292b8c422217b178230a13aa565df4
TiffyAI <quentenmeyer@gmail.com>
2025-09-02 02:09:26 +0200

    Update client.js

--- client.js
+++ client.js
@@ -1,25 +1,43 @@
 const socket = io();
-
-// Player state
 let playerId = null;
 let players = {};
 
-// On connect
+// Player avatar color palette
+const colors = ["#ff0000","#00ff00","#0000ff","#ffff00","#ff00ff","#00ffff"];
+
 socket.on("connect", () => {
   playerId = socket.id;
   console.log("Connected as:", playerId);
 });
 
-// Sync players
+// Update all players
 socket.on("updatePlayers", (data) => {
   players = data;
+  renderPlayers();
 });
 
-// Example movement loop
+// Render avatars
+function renderPlayers() {
+  const scene = document.querySelector('a-scene');
+  Object.keys(players).forEach(id => {
+    if (!scene.querySelector(`#player-${id}`)) {
+      const entity = document.createElement('a-sphere');
+      entity.setAttribute('id', `player-${id}`);
+      entity.setAttribute('radius', '0.5');
+      entity.setAttribute('color', colors[Math.floor(Math.random()*colors.length)]);
+      entity.setAttribute('position', `${players[id].x} ${players[id].y} ${players[id].z}`);
+      scene.appendChild(entity);
+    } else {
+      const entity = scene.querySelector(`#player-${id}`);
+      entity.setAttribute('position', `${players[id].x} ${players[id].y} ${players[id].z}`);
+    }
+  });
+}
+
+// Send movement updates
 setInterval(() => {
   const player = document.querySelector("#player");
   if (!player) return;
-
   const pos = player.object3D.position;
   socket.emit("move", { x: pos.x, y: pos.y, z: pos.z });
 }, 100);
